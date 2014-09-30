3e91f26c3223f4149f9120e5144b2299da61ea52
Todd Baginski <tbag@Todds-MacBook-Pro.local>
2014-09-30 13:36:24 -0600

    added code to synchronize user defaults

--- iOSNativeiPadApp/EdKeyNote/EdKeyNote/EdKeyNote/EKNAppDelegate.m
+++ iOSNativeiPadApp/EdKeyNote/EdKeyNote/EdKeyNote/EKNAppDelegate.m
@@ -77,6 +77,7 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    //[[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -87,6 +88,7 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
