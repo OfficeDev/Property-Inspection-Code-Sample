7ad1a3d6f54a5865b6d2b1eea4d9c43c7261c085
Todd Baginski <v-toddba@microsoft.com>
2014-10-22 23:28:21 -0600

    deleting old version of iOS-RepairApp

--- iOS-InspectionApp/ExchangeSdk/office365_exchange_sdk/office365_exchange_sdk/odata/header/com/microsoft/office365/exchange/services/odata/ODataOperations.h
+++
@@ -1,8 +0,0 @@
-#import "ODataExecutable.h"
-#import <office365_odata_interfaces/DependencyResolver.h>
-
-@interface ODataOperations : ODataExecutable
-
--(id)initOperationWith : (NSString*) urlComponent : (ODataExecutable*) parent;
-
-@end
