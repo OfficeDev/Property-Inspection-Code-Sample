5616c2c9f00f3f3fad0b81ca958f76461796d356
Todd Baginski <v-toddba@microsoft.com>
2014-12-05 12:23:17 -0700

    More additions to My App

---
+++ PropertyManagerMyApp/PropertyManagerMyApp/Utils/ServiceResources.cs
@@ -0,0 +1,11 @@
+﻿using System.Configuration;
+
+namespace SuiteLevelWebApp.Utils
+{
+    public static class ServiceResources
+    {
+        public static readonly string DemoSite = ConfigurationManager.AppSettings["DemoSiteServiceResourceId"];
+        public static readonly string Admin = ConfigurationManager.AppSettings["ServiceResourceId"];
+        public static readonly string Dashboard = ConfigurationManager.AppSettings["DashboardServiceResourceId"];
+    }
+}
