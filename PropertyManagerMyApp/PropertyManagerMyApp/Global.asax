a96e5b18a43e0d6ea50c21398d88b9a83f1df865
Todd Baginski <topshelftbag@hotmail.com>
2015-10-07 11:36:03 -0400

    Removing previous version of web app

--- PropertyManagerMyApp/PropertyManagerMyApp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SuiteLevelWebApp.MvcApplication" Language="C#" %>
--- PropertyManagerMyApp/PropertyManagerMyApp/Models/RepairCompletionModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SuiteLevelWebApp.Models
-{
-    public class RepairCompletionModel
-    {
-        public int IncidentId { get; set; }
-    }
-}
